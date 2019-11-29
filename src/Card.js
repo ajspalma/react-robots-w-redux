41a20d7775c65fe48cb3b7db220709df4e534505
ajspalma <palma.ashleyjs@gmail.com>
2019-11-29 22:33:50 +0800

    using props in card component with robots list (JSON object), and template literals (template strings) in props.id for robohash API

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src="https://robohash.org/test?200x200" alt="robots" />
+            <img src={`https://robohash.org/${ props.id }?200x200`} alt="robots" />
             <div>
-                <h2>Jane Doe</h2>
-                <h2>jane.doe@gmail.com</h2>
+                <h2>{props.name}</h2>
+                <h2>{props.email}</h2>
             </div>
         </div>
     );
