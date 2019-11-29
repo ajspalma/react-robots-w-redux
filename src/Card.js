3f86b25c8787e3a0a9b12d45c206daa806786201
ajspalma <palma.ashleyjs@gmail.com>
2019-11-29 22:43:47 +0800

    Simpler/shorthand destructuring (directly as parameters)

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-const Card = (props) => {
-    // destructuring
-    const { id, name, email } = props;
+const Card = ({ id, name, email }) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src={`https://robohash.org/${ id }?200x200`} alt="robots" />
