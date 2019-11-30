6562e9bf53c20430af030f2e941d05b9793e3afe
ajspalma <palma.ashleyjs@gmail.com>
2019-11-30 11:28:07 +0800

    Added key prop in each Card to have a unique identity for each component and to avoid errors given by react, and also fix spaces/indentation set to 2, and put the mapping direclty inside the return/render

--- src/CardList.js
+++ src/CardList.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const cardsArray = robots.map((user, i) => {
-        return <Card id={user.id} name={user.name} email={user.email} />
-    });
-    return (
-        <div>
-            { cardsArray }
-        </div>
-    );
+  return (
+    <div>
+      { 
+        robots.map((user, i) => {
+          return (
+            <Card 
+              key={i} 
+              id={user.id} 
+              name={user.name} 
+              email={user.email} 
+            />
+          );
+        })
+      }
+    </div>
+  );
 }
 
 export default CardList;
