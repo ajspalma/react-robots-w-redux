99bb695180a605ffff0cb7fa6a0b68f33c1b09cd
ajspalma <palma.ashleyjs@gmail.com>
2019-12-02 21:19:20 +0800

    converted App function to Class and extend Component from react package to use state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 import { robots } from './robots';
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>Robots</h1>
-      <SearchBox />
-      <CardList robots={ robots } />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: '',
+    }
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>Robots</h1>
+        <SearchBox />
+        <CardList robots={ this.state.robots } />
+      </div>
+    );
+  }
 }
 
 export default App;
