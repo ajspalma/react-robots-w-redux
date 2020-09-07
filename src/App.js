165dd3f758bf8851509aecbb29b2c4d129f654cb
ajspalma <palma.ashleyjs@gmail.com>
2020-09-07 22:57:58 +0800

    Using React CHILDREN, added Scroll Component and placed CardList Component (as its children) inside of it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
+import Scroll from './Scroll';
 import './App.css'; 
 
 class App extends Component {
@@ -24,7 +25,9 @@ class App extends Component {
       <div className="tc">
         <h1 className="f1">Robots</h1>
         <SearchBox searchChange={ this.onSearchChange }/>
-        <CardList robots={ filteredRobots } />
+        <Scroll>
+          <CardList robots={ filteredRobots } />
+        </Scroll>
       </div>
     );
   }
